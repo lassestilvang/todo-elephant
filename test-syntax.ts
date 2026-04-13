39bb803c633b0b58229e106316135c5a9994fea2
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-04-13 19:37:10 +0200

    test: add TypeScript syntax verification test file
    Creates test-syntax.ts to verify TypeScript syntax and that imported utilities from utils are valid.

---
+++ test-syntax.ts
@@ -0,0 +1,6 @@
+// Test file to verify TypeScript syntax
+import { focusTrap, useClickOutside } from './utils';
+
+// If this compiles, the syntax is correct
+const test: (() => void)[] = [];
+console.log('TypeScript syntax is valid');
